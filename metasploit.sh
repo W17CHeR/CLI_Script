3fc86bebd732a36829bd9639ec06d415c88dc76a
drake10010 <draak18@protonmail.com>
2024-12-09 17:03:37 +0100

    metasploit.sh
    Framework para la penetration de sistema, para más información:
    
    https://es.wikipedia.org/wiki/Metasploit

---
+++ metasploit.sh
@@ -0,0 +1,20 @@
+# ---- AVISO: Este Script esta desarrollado para fines educativos con lo que 
+# ---- me refiero a utilizarlo con cabeza (ética) no me hago responsable
+# ---- del mal uso de esté Script. -----------------------------------------
+# ---- Solo comparto como realizar una interfaz por línea de comandos-------
+# ---- Aquí comienza el Script: --------------------------------------------
+clear
+toilet --filter border "METASPLOIT";
+read -p "Pulse [ENTER] para ejecutar o ctrl + C para cancelar";
+clear
+ 
+for program in 3 2 1;
+do
+clear
+toilet --metal "$program";
+sleep 1
+done
+clear
+msfconsole
+ 
+# by W17CHER
