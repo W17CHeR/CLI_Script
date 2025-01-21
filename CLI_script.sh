34e3cb1a6a803a3ec0257cfe5c12658166d6179e
W17CH3R <draak18@protonmail.com>
2025-01-21 23:30:42 +0100

    Update CLI_script.sh
    * Añadido el antivirus Clamav a CLI_Script
    * Añadida fuente de color de figlet al antivirus.sh, ahora se vera la presentación de ClamAv en rojoo con 2 barras verdes.
    * Se ha eliminado el código de búsqueda de virus en analisis.sh, no era demasiado eficaz.
    * Ahora el script de analisis.sh se limitara en buscar solamente a intrusos conectados en tu equipo.
    * Corregido el código de analisis.sh, ahora cuando finalice te regresara al menu principal.
    * Corregido el orden de los scrits en CLI_Script, ahora en vez de 8 herramientas hay 9.
    * Corregido bug en CLI_Script que se interrumpia la ejecución del mismo.

--- CLI_script.sh
+++ CLI_script.sh
@@ -26,26 +26,28 @@ echo "----------------------------------------------------------------------";
 toilet --metal "Opciones";
 echo "----------------------------------------------------------------------";
 echo "1. Información del sistema";
-echo "2. Busqueda de intrusos y virus";
-echo "3. Antirootkit";
-echo "4. Vaciar papelera";
-echo "5. Ver en tiempo real los puertos salientes";
-echo "6. Ejecutar Metasploit";
-echo "7. Ejecutar Wireshark.sh";
-echo "8. Salir completamente del programa";
-echo -n "Eliga una opcion entre [1 - 8] o para cancelar use atajo Ctrl + C ";
+echo "2. Busqueda de intrusos";
+echo "3. Antivirus ClamAv con análisis completo del sistema";
+echo "4. Antirootkit";
+echo "5. Vaciar papelera";
+echo "6. Ver en tiempo real los puertos salientes";
+echo "7. Ejecutar Metasploit";
+echo "8. Ejecutar Wireshark.sh";
+echo "9. Salir completamente del programa";
+echo -n "Eliga una opcion entre [1 - 9] o para cancelar use atajo Ctrl + C ";
 	read opcion
 	case $opcion in
 1) /bin/bash specs.sh;;
 2) /bin/bash analisis.sh;;
-3) /bin/bash antirootkit.sh;;
-4) /bin/bash clean.sh;;
-5) /bin/bash network.sh;;
-6) . metasploit.sh;;
-7) /bin/bash wireshark.sh;;
-8)clear & toilet --metal "Hasta luego";;
-#exit 0;;
-*)echo "Solamente opciones del 1 al 8";;
+3) /bin/bash antivirus.sh;;
+4) /bin/bash antirootkit.sh;;
+5) /bin/bash clean.sh;;
+6) /bin/bash network.sh;;
+7) . metasploit.sh;;
+8) /bin/bash wireshark.sh;;
+9)clear & toilet --metal "Hasta luego";;
+
+*)echo "Solamente opciones del 1 al 9";;
 esac
 
-# by Sergio conocido en ElHacker.net como (W17CH3R) y en gitHub como (drake10010)
+# by Sergio conocido en ElHacker.net como (W17CH3R) y en gitHub como (W17CHeR)
